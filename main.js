eb4453254f6e12259268b9d49b7a07463d67871a
Pedro Dias <petermdias@gmail.com>
2014-07-29 14:29:39 +0100

    mongo

--- main.js
+++ main.js
@@ -6,31 +6,46 @@ var mongoClient = require('mongodb').MongoClient,
 var hipchatter = new Hipchatter(process.env.HIPCHAT_TOKEN);
 
 
-mongopubsub.subscribe('events', function (event) {
-  var opts;
-  console.log(event);
-  if(event.type == 'trigger' && event.level == 'alarmed') {
-    opts = {
-      message: 'Server ' + event.hostname + ' ' + event.level + ' with ' + event.value + ' ' + event.sensor,
-      color: 'red',
-      token: process.env.HIPCHAT_TOKEN_ROOM
-    };
-    hipchatter.notify(process.env.HIPCHAT_ROOM, opts, function(err){});
-  } else if(event.type == 'feed') {
-    opts = {
-      message: 'Feed ' + event.feed + ' reporting ' + event.url,
-      color: 'red',
-      token: process.env.HIPCHAT_TOKEN_ROOM
-    };
-    hipchatter.notify(process.env.HIPCHAT_ROOM, opts, function(err){});
+mongoClient.connect('mongodb://' + config.mongo_host + ':' + config.mongo_port + '/' + config.mongo_database, function(err, conn) {
+  if(err){
+    console.log(err.message);
+    throw new Error(err);
+  } else {
+    db = conn;
+    var channel = mubsub(db).channel('pubsub');
+    channel.on('error', console.error);
+    main(channel);
   }
 });
 
 
-/*
-mongopubsub.subscribe('messages', function (message) {
-  console.log(message);
-});
-*/
+function main(mongopubsub) {
+  var opts;
+  mongopubsub.subscribe('events', function (event) {
+    console.log(event);
+    if(event.type == 'trigger' && event.level == 'alarmed') {
+      opts = {
+        message: 'Server ' + event.hostname + ' ' + event.level + ' with ' + event.value + ' ' + event.sensor,
+        color: 'red',
+        token: process.env.HIPCHAT_TOKEN_ROOM
+      };
+      hipchatter.notify(process.env.HIPCHAT_ROOM, opts, function(err){});
+    } else if(event.type == 'feed') {
+      opts = {
+        message: 'Feed ' + event.feed + ' reporting ' + event.url,
+        color: 'red',
+        token: process.env.HIPCHAT_TOKEN_ROOM
+      };
+      hipchatter.notify(process.env.HIPCHAT_ROOM, opts, function(err){});
+    }
+  });
 
-console.log('Notifier started.');
+
+  /*
+  mongopubsub.subscribe('messages', function (message) {
+    console.log(message);
+  });
+  */
+
+  console.log('Notifier started.');
+}
