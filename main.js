a2a780f0f527901544f61679f8d948dc947145da
Pedro Dias <petermdias@gmail.com>
2014-08-13 16:00:35 +0100

    ver

--- main.js
+++ main.js
@@ -53,16 +53,18 @@ function main(db) {
 
   setInterval(function() {
     hipchatter.history(process.env.HIPCHAT_ROOM, function(err, history) {
-      var aux = history.items.filter(function(el) {
-        return el.from !== 'Jarvis' && el.from !== 'Icinga' && new Date(el.date).getTime() > last && el.message.toLowerCase().indexOf('@jarvis ') !== -1;
-      });
-      if(aux.length > 0) {
-        last = new Date(aux[aux.length - 1].date).getTime();
-        for (var i = 0; i < aux.length; i++) {
-          processMsg(aux[i]);
+      if(history && history.items) {
+        var aux = history.items.filter(function(el) {
+          return el.from !== 'Jarvis' && el.from !== 'Icinga' && new Date(el.date).getTime() > last && el.message.toLowerCase().indexOf('@jarvis ') !== -1;
+        });
+        if(aux.length > 0) {
+          last = new Date(aux[aux.length - 1].date).getTime();
+          for (var i = 0; i < aux.length; i++) {
+            processMsg(aux[i]);
+          }
         }
+        //console.log(aux);
       }
-      //console.log(aux);
     });
   }, 15000);
 
